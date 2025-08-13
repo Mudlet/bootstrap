657c1a0084b6254301f23ac91b1eb6f6070e3dc1
John McKisson <john.mckisson@gmail.com>
2025-08-13 17:17:21 -0400

    cmake path debugging

--- CI/build-win.sh
+++ CI/build-win.sh
@@ -92,9 +92,10 @@ while IFS= read -r line || [[ -n "$line" ]]; do
 
   echo "Running CMake configure..."
   ls ${QT_DIR}/Qt6Config.cmake
+  ls C:/a/_temp/msys64/mingw64
 
   echo "cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=$Qt6_PREFIX;/mingw64 .."
-  cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=$Qt6_PREFIX;/mingw64 ..
+  cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="$Qt6_PREFIX;C:\a\_temp\msys64\mingw64" ..
 
   echo "Building.."
   ninja
