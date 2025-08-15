2dbbaad0369f6dcdbd9c5c7fb4fef7d96d9c8fb3
John McKisson <john.mckisson@gmail.com>
2025-08-15 11:25:44 -0400

    try fixes from local msys2 work

--- CI/compile-qt-win.sh
+++ CI/compile-qt-win.sh
@@ -7,47 +7,25 @@ cd qt6-source
 git submodule update --init qtbase
 
 echo "=== Configuring Qt for Static Linking ==="
-perl init-repository --module-subset=qtbase,qttools,qttranslations
+#perl init-repository --module-subset=qtbase
 cd ${RUNNER_WORKSPACE}
 mkdir qt-static-build
 cd qt-static-build
 export CMAKE_SUPPRESS_DEVELOPER_WARNINGS=ON
 
 # Configure Qt with proper static dependencies
-../qt6-source/configure \
-    -prefix ${RUNNER_WORKSPACE}/qt-static-install \
-    -static \
-    -static-runtime \
-    -release \
-    -opensource \
-    -no-shared \
-    -confirm-license \
-    -submodules qtbase \
-    -nomake tests \
-    -nomake examples \
-    -skip qt3d \
-    -skip qtmultimedia \
-    -skip qtdeclarative \
-    -skip qtshadertools \
-    -skip qtquick \
-    -skip designer \
-    -no-opengl \
-    -no-dbus \
-    -platform win32-g++ \
-    -openssl-linked \
-    -system-pcre \
-    -system-zlib \
-    -system-libpng \
-    -system-libjpeg \
-    -system-freetype \
-    -system-harfbuzz \
-    -- \
-    -DFEATURE_system_pcre2=ON \
-    -DFEATURE_system_zlib=ON \
-    -DFEATURE_system_png=ON \
-    -DFEATURE_system_jpeg=ON \
-    -DFEATURE_system_freetype=ON \
-    -DFEATURE_system_harfbuzz=ON
+../qt6-source/configure -prefix ${RUNNER_WORKSPACE}/qt-static-install \
+  -static -static-runtime -release -opensource -no-shared -confirm-license \
+  -init-submodules -submodules qtbase \
+  -nomake tests -nomake examples \
+  -skip qt3d -skip qtmultimedia -skip qtdeclarative -skip qtshadertools -skip qtquick -skip designer \
+  -no-opengl -no-dbus \
+  -platform win32-g++ \
+  -qt-pcre \
+  -openssl-linked \
+  -- \
+  -DFEATURE_system_pcre2=OFF \
+
 
 #../qt6-source/configure -prefix ${RUNNER_WORKSPACE}/qt-static-install -static -static-runtime -release -opensource -no-shared -confirm-license -submodules qtbase -nomake tests -nomake examples -skip qt3d -skip qtmultimedia -skip qtdeclarative -skip qtshadertools -skip qtquick -skip designer -no-opengl -no-dbus -platform win32-g++ -openssl-linked
 
