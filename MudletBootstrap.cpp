63345da445e8addfc089fcb245cc22ee32a0efa6
John McKisson <john.mckisson@gmail.com>
2025-09-03 19:55:35 -0400

    Create autologin file for the launcher profile (#19)
    * create autologin file

--- MudletBootstrap.cpp
+++ MudletBootstrap.cpp
@@ -16,6 +16,7 @@
 #include <QJsonArray>
 #include <QMap>
 #include <QStandardPaths>
+#include <QVersionNumber>
 
 QMap<QString, QString> getPlatformFeedMap(const QString &type) {
 
@@ -571,8 +572,11 @@ bool installAndRunAppImage(QProcessEnvironment &env, const QString& tarFilePath)
 
 /**
  * @brief Platform agnostic install steps.
- * Sets up MUDLET_PROFILES env variable from launch.ini, runs the platform specific
- * installer. Emits corresponding installComplete or errorOccurred state signal.
+ * - Sets up MUDLET_PROFILES env variable from launch.ini
+ * - Installs autologin file for the wanted profile in users home/.config/Mudlet/<profile>
+ * - runs the platform specific installer.
+ * 
+ * Emits corresponding installComplete or errorOccurred state signal.
  * 
  */
 void MudletBootstrap::installApplication() {
@@ -588,6 +592,43 @@ void MudletBootstrap::installApplication() {
     } else {
         // Pass along the launch profile to the environment
         env.insert("MUDLET_PROFILES", launchProfile);
+    }
+
+    // Create autologin file for the wanted profile
+    QString confDirDefault = QDir::homePath() + 
+        QDir::separator() + ".config" +
+        QDir::separator() + "mudlet" +
+        QDir::separator() + "profiles" + 
+        QDir::separator() + launchProfile;
+    QDir configDir;
+    if (!configDir.mkpath(confDirDefault)) {
+        qDebug() << "Failed to create config directory:" << confDirDefault;
+    } else {
+        // Create the autologin file
+        QString autologinFilePath = confDirDefault + QDir::separator() + "autologin";
+        QFile autologinFile(autologinFilePath);
+
+        // A constant equivalent to QDataStream::Qt_5_12 needed in several places
+        // which can't be pulled from Qt as it is not going to be defined for older
+        // versions:
+        static const int scmQDataStreamFormat_5_12 = 18;
+
+        if (autologinFile.open(QIODevice::WriteOnly)) {
+            QDataStream out(&autologinFile);
+            
+            // Set the same data stream version that Mudlet uses for reading
+            if (QVersionNumber::fromString(qVersion()) >= QVersionNumber(5, 13, 0)) {
+                out.setVersion(scmQDataStreamFormat_5_12);
+            }
+
+            QString autologinData = QString::number(Qt::Checked);
+            out << autologinData;
+            
+            autologinFile.close();
+            qDebug() << "Autologin file created successfully:" << autologinFilePath;
+        } else {
+            qWarning() << "Failed to create autologin file:" << autologinFilePath << autologinFile.errorString();
+        }
     }
 
     statusLabel->setText(QString("Installing %1").arg(info.appName));
