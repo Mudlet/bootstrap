a5f59cb0ad0523726b0076dcece8a2aa46473dae
Vadim Peretokin <vperetokin@hey.com>
2025-09-03 22:02:30 +0200

    Move MB information into the progress bar

--- MudletBootstrap.cpp
+++ MudletBootstrap.cpp
@@ -357,11 +357,15 @@ void MudletBootstrap::onDownloadProgress(qint64 bytesReceived, qint64 bytesTotal
     if (bytesTotal > 0) {
         int progress = static_cast<int>((bytesReceived * 100) / bytesTotal);
         progressBar->setValue(progress);
+        
+        // Set MB progress text on the progress bar
+        progressBar->setFormat(QString("%1 / %2 MB")
+            .arg(bytesReceived/1048576.0, 0, 'f', 2)
+            .arg(bytesTotal/1048576.0, 0, 'f', 2));
     }
-    statusLabel->setText(QString("Downloading %1... %2 / %3 MB")
-        .arg(info.appName)
-        .arg(bytesReceived/1048576.0, 0, 'f', 2)
-        .arg(bytesTotal/1048576.0, 0, 'f', 2));
+    
+    // Keep status label simple
+    statusLabel->setText(QString("Downloading %1...").arg(info.appName));
 }
 
 
