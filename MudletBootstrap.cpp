361e6a581ced5ec07c7dc70bfc0e3ed5d7bda95c
Vadim Peretokin <vperetokin@hey.com>
2025-09-05 16:03:13 +0200

    Fix syntax

--- MudletBootstrap.cpp
+++ MudletBootstrap.cpp
@@ -370,7 +370,7 @@ void MudletBootstrap::onDownloadProgress(qint64 bytesReceived, qint64 bytesTotal
             .arg(bytesReceived/1048576.0, 0, 'f', 2)
             .arg(bytesTotal/1048576.0, 0, 'f', 2));
     }
-    statusLabel->setText(QString("Downloading Mudlet for %1...");
+    statusLabel->setText(QString("Downloading Mudlet for %1..."));
 }
 
 
